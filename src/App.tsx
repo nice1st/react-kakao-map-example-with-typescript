5efd13f6b6d471ce441c038bcc3674b8ef749cb1
nice1st <sasimi86cyh@gmail.com>
2020-05-26 11:14:35 +0900

    init map

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
 function App() {
+  useEffect(() => {
+    let container = document.getElementById("map");
+    let options = {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667)
+      , level: 3
+    };
+
+    let map = new window.kakao.maps.Map(container, options);
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="map" style={{width: "100vw", height: "100vh"}} />
     </div>
   );
 }
